cb749e49421fb978ce7dbe401b0d560520069aa3
David Fowler <davidfowl@gmail.com>
2016-10-11 21:28:55 -0700

    Initial commit

---
+++ src/WebApplication1/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public static class MyModule
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public static void AddHelloServices(this IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public static void UseHello(this IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+
+    public class Home2Controller
+    {
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "Hello World MVC Module1";
+        }
+    }
+}
---
+++ src/WebApplication98/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using WebApplication1;
+
+namespace WebApplication98
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var copy = new ServiceCollection();
+            foreach (var sd in services)
+            {
+                copy.Add(sd);
+            }
+
+            services.AddSingleton<IServiceCollection>(copy);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IServiceCollection hostingServices, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Map("/hello", subApp =>
+            {
+                var moduleEnv = new HostingEnvironment();
+                moduleEnv.EnvironmentName = env.EnvironmentName;
+                moduleEnv.ApplicationName = "WebApplication1";
+                moduleEnv.ContentRootPath = Path.Combine(env.ContentRootPath, @"..", "WebApplication1");
+                // TODO: Support this in hosting
+                // moduleEnv.PathBase = "/hello";
+
+
+                // Add the default hosting services every ASP.NET application should have
+                var services = new ServiceCollection();
+                foreach (var sd in hostingServices)
+                {
+                    services.Add(sd);
+                }
+
+                services.AddSingleton(moduleEnv);
+                services.AddSingleton(loggerFactory);
+                services.AddHelloServices();
+
+                // Make a child service provider
+                subApp.ApplicationServices = services.BuildServiceProvider();
+
+                // Run the module middleware
+                subApp.UseHello();
+            });
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
